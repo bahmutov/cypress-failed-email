945498a64d305fe2eb3dac5d22b1e0d6e4d1463d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-30 17:31:53 -0500

    feat(email): testing generated email

--- src/cypress-failed-email-spec.js
+++ src/cypress-failed-email-spec.js
@@ -1,9 +1,34 @@
 'use strict'
 
+const la = require('lazy-ass')
+const is = require('check-more-types')
+
 /* global describe, it */
 describe('cypress-failed-email', () => {
-  const cypressFailedEmail = require('.')
-  it('write this test', () => {
-    console.assert(cypressFailedEmail, 'should export something')
+  const {formEmail} = require('.')
+
+  const failed = {
+    title: "title",
+    testName: "suite title",
+    testError: "a test fails",
+    testCommands: [
+      'visit /',
+      'fails 100%'
+    ]
+  }
+  const mockGitLabCIenv = {
+    CI: 1,
+    CI_PROJECT_URL: 'https://server/project',
+    CI_BUILD_NAME: 'cypress-failed-email'
+  }
+
+  it('forms email without build info', () => {
+    const email = formEmail(failed, mockGitLabCIenv)
+    const {subject, plainText} = email
+    la(is.unemptyString(subject), 'missing email text in', email)
+    la(is.unemptyString(plainText), 'missing plain text in', email)
+
+    // console.log(subject)
+    // console.log(plainText)
   })
 })
--- src/index.js
+++ src/index.js
@@ -3,21 +3,34 @@
 const la = require('lazy-ass')
 const is = require('check-more-types')
 const os = require('os')
-const gitlab = require('gitlab-build-info')(process.env)
+const gitlabInit = require('gitlab-build-info')
 // TODO add other CIs: Travis, Jenkins
 
 const osName = `OS: ${os.platform()} - ${os.hostname()} - ${os.release()}`
 
 const hasTestName = failed => is.unemptyString(failed.testName)
 
+const formSubject = (failed, buildInfo) => {
+  la(is.unemptyString(failed.testName), 'missing test name in', failed)
+  const start = `Oh NO!!! Failed test "${failed.testName}"`
+
+  const project = buildInfo.projectName
+  const build = buildInfo.buildName
+  if (project && build) {
+    return `${start} - build ${project}:${build}`
+  }
+  return start
+}
+
 // failed: information collected by "cypress-failed-log"
-function formEmail (failed) {
+function formEmail (failed, env = process.env) {
   la(is.object(failed),
     'missing failed test contents', failed)
   la(hasTestName(failed), 'missing test name', failed)
   la(is.strings(failed.testCommands), 'missing test commands log', failed)
   la(is.unemptyString(failed.testError), 'missing failed test error', failed)
 
+  const gitlab = gitlabInit(env)
   const buildInfo = gitlab.collectionInformation()
   // TODO support emails without any build information
   la(is.object(buildInfo), 'missing build info')
@@ -26,8 +39,8 @@ function formEmail (failed) {
 This is automated email - one of end to end tests has failed.
 Please do not hate me - I am just a messenger; fix your stuff.
 
-Details are below.
-==================
+ Details are below
+===================
 Test name: "${failed.title}"
 Test full name: "${failed.testName}"
 Error: "${failed.testError}"
@@ -46,9 +59,7 @@ Error: "${failed.testError}"
   plainText += osName
   plainText += '\n' + testCommandsLog
 
-  const subject = buildInfo.buildName
-  ? `Oh NO!!! Failed test ${buildInfo.projectName}:${buildInfo.buildName}`
-  : `Oh NO!!! Failed test ${buildInfo.projectName}:${failed.testName}`
+  const subject = formSubject(failed, buildInfo)
 
   return {
     plainText,
